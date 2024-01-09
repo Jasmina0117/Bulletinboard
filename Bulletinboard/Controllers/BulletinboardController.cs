af7054e4e4be20e29c91172637f35475823ba0ff
Jasmina1998 <51716464+Jasmina1998@users.noreply.github.com>
2024-01-09 10:25:48 +0800

    feat. 加入swaager/ DB

---
+++ Bulletinboard/Controllers/BulletinboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bulletinboard.Controllers
+{
+    public class BulletinboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Bulletinboard/Models/Announcement.cs
@@ -0,0 +1,11 @@
+﻿namespace Bulletinboard.Models
+{
+    public class Announcement
+    {
+        public int AnnouncementID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Publisher { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
